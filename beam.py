0d88cbc7e482e0a405a19e3b48bf4ccf2b58e014
gkennedy <graeme.kennedy@ae.gatech.edu>
2024-04-11 14:23:43 -0400

    added more to beam code

--- beam.py
+++ beam.py
@@ -11,17 +11,37 @@ from scipy.sparse import coo_matrix, linalg
 
 
 class Truss:
-    def __init__(self, conn, xpts, E=1e2, A=1.0, bcs=[]):
+    def __init__(
+        self,
+        conn,
+        xpts,
+        E=1.0,
+        rg=1.0,
+        hdof=0,
+        N=10,
+        bcs=[],
+        forces={},
+    ):
         self.conn = np.array(conn, dtype=int)
         self.xpts = np.array(xpts, dtype=float)
         self.nelems = len(self.conn)
         self.bcs = bcs
 
+        # Degree of freedom to use as a relative modal displacement constraint
+        self.hdof = hdof
+
+        # Number of eigenvectors to compute
+        self.N = N
+
         # Set the number of degrees of freedom - 3 dof at each node
         self.ndof = 3 * (np.max(self.conn) + 1)
 
-        self.E = E
-        self.rg = 1  # Radius of gyration
+        self.E = E  # Elastic modulus
+        self.rg = rg  # Radius of gyration
+
+        # Set the reduced set of forces
+        self.reduced = self._compute_reduced_variables(self.nvndofars, bcs)
+        self.f = self._compute_forces(self.ndof, forces)
 
         # Set up the i-j indices for the matrix - these are the row
         # and column indices in the stiffness matrix
@@ -43,7 +63,60 @@ class Truss:
 
         return
 
-    def get_element_transform(self, elem):
+    def _compute_reduced_variables(self, ndof, bcs):
+        """
+        Compute the reduced set of variables
+        """
+        reduced = list(range(ndof))
+
+        # For each node that is in the boundary condition dictionary
+        for node in bcs:
+            dof_list = bcs[node]
+
+            # For each index in the boundary conditions (corresponding to
+            # either a constraint on u and/or constraint on v
+            for index in dof_list:
+                var = 3 * node + index
+                reduced.remove(var)
+
+        return reduced
+
+    def _reduce_vector(self, forces):
+        """
+        Eliminate essential boundary conditions from the vector
+        """
+        return forces[self.reduced]
+
+    def _reduce_matrix(self, matrix):
+        """
+        Eliminate essential boundary conditions from the matrix
+        """
+        temp = matrix[self.reduced, :]
+        return temp[:, self.reduced]
+
+    def _full_vector(self, vec):
+        """
+        Transform from a reduced vector without dirichlet BCs to the full vector
+        """
+        if vec.ndim == 1:
+            temp = np.zeros(self.ndof, dtype=vec.dtype)
+            temp[self.reduced, :] = vec[:]
+        elif vec.ndim == 2:
+            temp = np.zeros((self.ndof, vec.shape[1]), dtype=vec.dtype)
+            temp[self.reduced, :] = vec[:]
+        return temp
+
+    def _full_matrix(self, mat):
+        """
+        Transform from a reduced matrix without dirichlet BCs to the full matrix
+        """
+        temp = np.zeros((self.ndof, self.ndof), dtype=mat.dtype)
+        for i in range(len(self.reduced)):
+            for j in range(len(self.reduced)):
+                temp[self.reduced[i], self.reduced[j]] = mat[i, j]
+        return temp
+
+    def _get_element_transform(self, elem):
         n1 = self.conn[elem, 0]
         n2 = self.conn[elem, 1]
         dx = self.xpts[n2, 0] - self.xpts[n1, 0]
@@ -57,7 +130,7 @@ class Truss:
 
         return L, T
 
-    def get_element_stifness_matrix(self, elem, area):
+    def _get_element_stifness_matrix(self, elem, area):
         """Compute the element stiffness matrix"""
 
         L, T = self.get_element_transform(elem)
@@ -103,7 +176,7 @@ class Truss:
 
         return Te.T @ Ke @ Te
 
-    def get_element_stress_stifness_matrix(self, elem, u, area):
+    def _get_element_stress_stifness_matrix(self, elem, u, area):
         """Compute the element stiffness matrix"""
 
         L, T = self.get_element_transform(elem)
@@ -147,22 +220,22 @@ class Truss:
 
         return Te.T @ Ge @ Te
 
-    def assemble_stiffness_matrix(self, x):
+    def _assemble_stiffness_matrix(self, x):
         Ke = np.zeros((self.nelems, 6, 6))
 
         for elem in range(self.nelems):
-            Ke[elem, :, :] = self.get_element_stifness_matrix(elem, x[elem])
+            Ke[elem, :, :] = self._get_element_stifness_matrix(elem, x[elem])
 
         K = sparse.coo_matrix((Ke.flatten(), (self.i, self.j)))
         K = K.tocsr()
 
         return K
 
-    def assemble_stress_stiffness_matrix(self, x, u):
+    def _assemble_stress_stiffness_matrix(self, x, u):
         Ge = np.zeros((self.nelems, 6, 6))
 
         for elem in range(self.nelems):
-            Ge[elem, :, :] = self.get_element_stress_stifness_matrix(
+            Ge[elem, :, :] = self._get_element_stress_stifness_matrix(
                 elem, x[elem], u[self.var[elem, :]]
             )
 
@@ -170,3 +243,45 @@ class Truss:
         G = G.tocsr()
 
         return G
+
+    def solve_eigenvalue_problem(self, x, sigma=1.0):
+
+        K0 = self._assemble_stiffness_matrix(x)
+        K = self._reduce_matrix(K0)
+
+        # Compute the solution path
+        fr = self._reduce_vector(self.f)
+        fact = linalg.factorized(K0)
+        u = self._full_vector(fact(fr))
+
+        G0 = self._assemble_stress_stiffness_matrix(x)
+        G = self._reduce_matrix(G0)
+
+        mu, Q = sparse.linalg.eigsh(
+            G,
+            M=K,
+            k=self.N,
+            sigma=sigma,
+            which="SM",
+            maxiter=1000,
+            tol=1e-8,
+        )
+
+        self.mu = mu
+        self.BLF = -1.0 / mu
+        self.Q = self._full_vector(Q)
+
+        return self.BLF, self.Q
+
+    def buckling_aggregate(self, x):
+
+        BLF, Q = self.solve_eigenvalue_problem(x)
+
+        eta = np.exp(-self.ks_rho * (BLF - np.min(BLF)))
+        eta = eta / np.sum(eta)
+
+        h = 0.0
+        for i in range(self.N):
+            h += eta[i] * Q[self.hdof] ** 2
+
+        return h
